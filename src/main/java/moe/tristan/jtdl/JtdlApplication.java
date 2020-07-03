e295de713d220a199961aaab11e33b3b727dfe2a
Tristan Deloche <tristan.deloche@treatwell.com>
2020-07-03 17:00:41 +0100

    Initial commit

---
+++ src/main/java/moe/tristan/jtdl/JtdlApplication.java
@@ -0,0 +1,13 @@
+package moe.tristan.jtdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JtdlApplication.class, args);
+    }
+
+}
